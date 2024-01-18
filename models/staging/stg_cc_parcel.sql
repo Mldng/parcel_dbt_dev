71ec216ac692a29a56367d3acf16b448db2dddff
Melody (Thuy Duong) Nguyen <melody.duong.nguyen@gmail.com>
2024-01-18 14:11:42 +0000

    revise stg model for parcel_product to add model_name

--- models/staging/stg_cc_parcel.sql
+++ models/staging/stg_cc_parcel.sql
@@ -1,5 +1,9 @@
+WITH stg_cc_parcel_product AS(
+    SELECT *
+    FROM {{ref("stg_cc_parcel_products")}}
+)
 SELECT
-Parcel_id as parcel_id
+cp.Parcel_id as parcel_id
 ,Parcel_tracking as parcel_tracking
 ,Transporter as transporter
 ,Priority as priority
@@ -7,4 +11,6 @@ Parcel_id as parcel_id
 ,PARSE_DATE("%B %e, %Y", Date_sHIpping) AS date_shipping
 ,PARSE_DATE("%B %e, %Y", DATE_delivery) AS date_delivery
 ,PARSE_DATE("%B %e, %Y", DaTeCANcelled) AS date_cancelled
-FROM raw_data_circle.raw_cc_parcel
+,stg_cc_parcel_product.model_name
+FROM raw_data_circle.raw_cc_parcel cp
+LEFT JOIN stg_cc_parcel_product ON stg_cc_parcel_product.parcel_id = cp.Parcel_id
